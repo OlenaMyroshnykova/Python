a2b3f8f01af841efdccb3c4508dff60abd9a3c14
OlenaMyroshnykova <elenamirespania@gmail.com>
2026-04-15 13:16:06 +0200

    Add influencer campaign dictionary example and update budget

--- diccionarios.py
+++ diccionarios.py
@@ -10,3 +10,18 @@
 # datos_perfil = datos_basicos.copy() #creamos una copia del primer diccionario para no modificarlo
 # datos_perfil.update(datos_extra) #fusionamos el segundo diccionario en la copia del primero, actualizando el nivel a 16
 # print(datos_perfil) #imprimimos el diccionario fusionado, que contiene los datos combinados de ambos diccionarios
+
+#Creamos la ficha tecnica de un influencer usando un diccionario
+campaña_verano = {
+    "cliente": "SuperSoda",
+    "presupuesto": 50000,
+    "redes": ["Instagram", "TikTok"],
+    "objetivo_kpi": "Aumentar seguidores",
+    "finalizada": False
+}
+#Pedimos los datos del cliente usando las claves del diccionario
+print(f"Campaña para el cliente: {campaña_verano['cliente']}")
+#Actualizamos el presupuesto de la campaña
+campaña_verano["presupuesto"] = 60000
+campaña_verano["responsable"] = "Sara"#Agregamos una nueva clave-valor al diccionario para indicar el responsable de la campaña
+print(f"Actualizacion de la campaña: {campaña_verano}")
