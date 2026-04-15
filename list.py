65d84d9ec0a47c42f3f6cf16ca172bbc394fa3e7
OlenaMyroshnykova <elenamirespania@gmail.com>
2026-04-15 12:11:36 +0200

    Refactor list.py to uncomment and organize color-related code examples

--- list.py
+++ list.py
@@ -1,1 +1,18 @@
+# invitados = ["Shakira", "Coldplay", "Beyoncé", "Stromae", "Camillo", "Lady Gaga"]
+# invitados.append("Bruno Mars")
+# invitados.remove("Beyoncé")
+# invitados_totales = len(invitados)
+# invitados.sort()
+# print(f"La lista final para la fiesta es: {invitados}")
+# print(f"Total de acreditaciones: {invitados_totales}")
 
+#Sumar precios de una lista
+# precios = [120, 200, 150, 300, 250]
+# total = sum(precios)
+# print(f"El total de los precios es: {total}")
+
+colores = ["rojo", "azul", "verde","rojo", "amarillo","rojo", "morado","rojo"]
+colores.insert(2, "naranja")
+total_rojo = colores.count("rojo")
+print(colores)
+print(f"El color rojo aparece {total_rojo} veces en la lista.")
